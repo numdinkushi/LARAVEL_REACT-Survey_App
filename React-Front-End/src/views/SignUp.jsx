6b6c1fbc7b9720d7c022719a070badc372b903f9
numdinkushi <numdinkushi@gmail.com>
2023-06-22 23:58:39 +0100

    added sign up

--- React-Front-End/src/views/SignUp.jsx
+++ React-Front-End/src/views/SignUp.jsx
@@ -1,16 +1,91 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios";
+import { useStateContext } from "../context/ContexProvider";
 export const SignUp = () => {
+  const {setCurrentUser, setUserToken} = useStateContext();
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState({ __html: "" });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setErrors({ __html: "" });
+
+    axiosClient
+      .post("/signup", {
+        name: fullName,
+        email: email,
+        password: password,
+        password_confirmation: passwordConfirmation,
+      })
+      .then(({ data }) => {
+        console.log(111, data)
+        setCurrentUser(data.user);
+        setUserToken(data.token);
+      })
+      .catch((error) => {
+        if (error.response) {
+          const finalErrors = Object.values(error.response.data.errors).reduce((accum, next) => [...accum, ...next], [])
+          console.log(finalErrors)
+          setErrors({__html: finalErrors.join('<br>')})
+        }
+
+        console.log(error);
+      });
+  };
   return (
     <>
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Sign up for free
       </h2>
+      <p className="text-center mt-2 text-sm text-gray-600">
+        Or{" "}
+        <Link
+          to="/login"
+          className="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          {" "}
+          Login with your account{" "}
+        </Link>
+      </p>
+ 
+      {errors.__html && (<div className="bg-red-500 rounded py-2 px-3 mt-2 text-white" dangerouslySetInnerHTML={errors}>
+      </div>)}
+
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form
+          onSubmit={onSubmit}
+          className="space-y-6"
+          action="#"
+          method="POST"
+        >
           <div>
             <label
               htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Full Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="name"
+                name="name"
+                value={fullName}
+                onChange={(event)=> setFullName(event.target.value)}
+                type="text"
+                placeholder="Full Name"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="Full Name"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Email address
@@ -20,13 +95,15 @@ export const SignUp = () => {
                 id="email"
                 name="email"
                 type="email"
+                value={email}
+                onChange={(event)=> setEmail(event.target.value)}
+                placeholder="abc@gmail.com"
                 autoComplete="email"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-
           <div>
             <div className="flex items-center justify-between">
               <label
@@ -34,6 +111,28 @@ export const SignUp = () => {
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                value={password}
+                onChange={(event)=> setPassword(event.target.value)}
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password-confirmation"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Confirm Passsword
               </label>
               <div className="text-sm">
                 <a
@@ -46,10 +145,12 @@ export const SignUp = () => {
             </div>
             <div className="mt-2">
               <input
-                id="password"
-                name="password"
+                id="password-confirmation"
+                name="password_confirmation"
+                value={passwordConfirmation}
+                onChange={(event)=> setPasswordConfirmation(event.target.value)}
                 type="password"
-                autoComplete="current-password"
+                placeholder="Password confirmation"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -61,20 +162,10 @@ export const SignUp = () => {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              Sign up
             </button>
           </div>
         </form>
-
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{" "}
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
       </div>
     </>
   );
